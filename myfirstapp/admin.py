051a2b941b9c3a61062723d3a06b03b491d629e9
unknown <arenmamyan3644@gmail.com>
2021-10-20 20:00:23 +0400

    Aren

--- myfirstapp/admin.py
+++ myfirstapp/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from .models import Person
+from datetime import datetime
 
-
-class PersonAdmin(admin.ModelAdmin):
-    verbose_name = "Имена"
-    list_display = ('name', 'surname', 'gender',)
-
-
-admin.site.register(Person, PersonAdmin)
-
-# @admin.register(Person)
-# class PersonAdmin(admin.ModelAdmin):
 #
-#     list_display = ('name', 'surname', 'gender')
+# class PersonAdmin(admin.ModelAdmin):
+#     verbose_name = "Имена"
+#     list_display = ('name', 'surname', 'gender',)
+
+
+# admin.site.register(Person, PersonAdmin)
+
+a = datetime.now()
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    def age(self, other):
+        return a.year - other.date_of_birth
+
+    list_display = ('name', 'surname', 'gender', 'age')
